1dc0b9887ac798d3a2810ac3fac5ab9fc6b124b8
Martin Löffler <git@fatalerrorcoded.eu>
2020-08-04 00:15:06 +0200

    Styling

--- src/menu.tsx
+++ src/menu.tsx
@@ -1,6 +1,28 @@
 import { h, JSX, ComponentChildren } from "preact";
 import { useEffect, useCallback } from "preact/hooks";
 
+import { StyleSheet, css } from "aphrodite";
+
+const styles = StyleSheet.create({
+    preact_context_menu: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden',
+        pointerEvents: 'none',
+    },
+    menu: {
+        pointerEvents: 'initial',
+        backgroundColor: "#FFFFFF",
+    }
+});
+
+const menuContainer = document.createElement("div");
+menuContainer.classList.add(css(styles.preact_context_menu));
+document.body.appendChild(menuContainer);
+
 export const contextMenus: Map<string, () => void> = new Map();
 
 type ContextMenuProps = Omit<JSX.HTMLAttributes<HTMLDivElement>, "id"> & {
